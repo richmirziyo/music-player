c82c65ea3ab0c41b9aab97138a269929b4fc434c
mirziyo <mirziyo>
2025-01-23 23:26:17 +0500

    refactor: update styles and add music progress functionality

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ const voice = document.getElementById("voice");
 const container = document.getElementById("container");
 const audioVolum = document.getElementById("audioVolum");
 const cover = document.getElementById("cover");
+const title = document.getElementById("title");
+const proccessContainer = document.querySelector(".proccess-container");
+const voiceProccess = document.getElementById("voice-proccess");
 
 let indexConten = 0;
 const musics = [
@@ -17,7 +20,8 @@ const musics = [
 
 function changeMusic(index) {
   cover.src = `../images/${musics[index]}.jpg`;
-  audio.src = `../music/${musics[index]}.mp3`;
+  audio.src = `./music/${musics[index]}.mp3`;
+  title.textContent = musics[index];
 }
 
 changeMusic(indexConten);
@@ -33,6 +37,33 @@ function nextMusic() {
   play();
 }
 
+// // Oldingi musiqa
+// function prevMusic() {
+//   // Musiqalar ro'yxatida bir qadam orqaga o'tamiz
+//   indexContent = (indexContent - 1 + musics.length) % musics.length;
+
+//   // Tanlangan musiqani yangilaymiz
+//   changeMusic(indexContent);
+
+//   // Musiqani o'ynatamiz
+//   play();
+// }
+
+// function prevMusic() {
+//   if (indexContent > 0) {
+//     indexContent = indexContent - 1; // Oldingi musiqa
+//   } else {
+//     indexContent = musics.length - 1; // Oxirgi musiqaga o'tish
+//   }
+//   changeMusic(indexContent);
+//   play();
+// }
+
+// function prevMusic() {
+//   indexContent = (indexContent - 1 + musics.length) % musics.length;
+//   changeMusic(indexContent);
+//   play();
+// }
 
 // // musiqani ortga qaytarish uchun
 // function prevMusic() {
@@ -52,6 +83,13 @@ voice.addEventListener("input", () => {
   audio.volume = voice.value / 100;
   audioVolum.textContent = voice.value;
 });
+
+audioVolum.style.cssText = `
+font-size:19px;
+font-family: "Lora";
+`;
+
+
 
 function play() {
   container.classList.add("playing");
@@ -75,5 +113,15 @@ playBtn.addEventListener("click", () => {
   }
 });
 
+function playingMusic() {
+  let duration = audio.duration;
+  let currentTime = audio.currentTime;
+  proccessContainer.style.width = `${(currentTime / duration) * 100}%`;
+}
+
+
+
 forwardBtn.addEventListener("click", nextMusic);
 // backwardBtn.addEventListener("click", prevMusic);
+audio.addEventListener("ended", nextMusic);
+audio.addEventListener("timeupdate", playingMusic);
